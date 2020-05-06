185e618cd0b4e304ce62ee845acf5f1de97ba235
TangSY <t@tsy6.com>
2020-05-06 21:54:04 +0800

    docs: 添加 被标注的日期下面进行备注功能 的demo。（#19）

---
+++ examples/SlotDemo.vue
@@ -0,0 +1,76 @@
+/**
+* @Description:    通过 slot 实现被标注的日期下面进行备注功能  github issues：https://github.com/TangSY/vue-hash-calendar/issues/19
+* @Author:         TSY
+* @CreateDate:     2019/05/23 00:08
+* @Email:          t@tsy6.com
+*/
+<template>
+  <div class="body">
+    <button @click="showCalendarDialog">显示</button>
+    <vue-hash-calendar ref="picker"
+                       model="dialog"
+                       :visible.sync="isShowCalendar"
+                       :mark-date="markDate"
+                       picker-type="date">
+      <div slot="day" slot-scope="scope">
+        <div>{{scope.date.day}}</div>
+        <div v-if="scope.extendAttr.isMarked">标记</div>
+      </div>
+    </vue-hash-calendar>
+    <!--github入口-->
+    <github></github>
+  </div>
+</template>
+
+<script>
+import Github from './Github.vue'
+
+export default {
+  name: 'demo',
+  components: { Github },
+  data() {
+    return {
+      isShowCalendar: true, // 是否显示弹窗
+      isShowTips: false, // 是否显示下载提示
+      defaultDatetime: new Date(),
+      markDate: [
+        '2020/11/24', '2020/11/22',
+        { color: 'red', date: ['0', '2020/02/25', '2020/03/25', '2020/04/01', '2020/05/25', '2020/06/25', '2020/07/25', '2020/08/25', '2020/09/25', '2020/10/25', '2020/11/25', '2020/12/25'] },
+        { color: 'blue', date: ['2020/01/20', '2020/02/20', '2020/03/20', '2020/04/20', '2020/05/20', '2020/06/20', '2020/07/20', '2020/08/20', '2020/09/20', '2020/10/20', '2020/11/20', '2020/12/20'] },
+        { color: 'pink', date: ['2020/01/12', '2020/02/12', '2020/03/12', '2020/04/12', '2020/05/12', '2020/06/12', '2020/07/12', '2020/08/12', '2020/09/12', '2020/10/12', '2020/11/12', '2020/12/12'] },
+        { color: '#000000', date: ['2020/01/29', '2020/02/29', '2020/03/29', '2020/04/29', '2020/05/29', '2020/06/29', '2020/07/29', '2020/08/29', '2020/09/29', '2020/10/29', '2020/11/29', '2020/12/29'] }
+      ] // 对象数组形式的标记日期，可以自定义标记颜色
+    }
+  },
+  mounted() {
+    //            this.defaultDatetime = new Date('2018-01-01 12:04');
+  },
+  methods: {
+    showCalendarDialog() { // 显示日历
+      this.isShowCalendar = true
+    },
+    dateChange(date) { // 日期改变触发
+      console.log(date, 'change')
+    },
+    dateConfirm(date) { // 点击确认按钮触发
+      console.log(date, 'confirm')
+    },
+    dateClick(date) { // 点击日期时按钮触发
+      console.log(date, 'click')
+    },
+    disabledDate(date) { // 禁用的日期
+      let timestamp = date.getTime()
+      let oneDay = 24 * 60 * 60 * 1000
+
+      if (timestamp < new Date().getTime() - oneDay) {
+        return true
+      }
+      return false
+    }
+  }
+}
+</script>
+<style lang="stylus">
+.hhhh
+  background red
+</style>
