7023043f45c96970183a3e76de0b8d1bc1a15bb3
HashTang <t@tsy6.com>
2021-05-28 22:36:49 +0800

    feat: 添加年月选择面板

--- packages/vueHashCalendar/src/YearMonthPicker.vue
+++ packages/vueHashCalendar/src/YearMonthPicker.vue
@@ -19,21 +19,21 @@ export default {
   },
   data() {
     return {
-      monthArr: [
-        { label: '一月', value: 0 }, { label: '二月', value: 1 }, { label: '三月', value: 2 },
-        { label: '四月', value: 3 }, { label: '五月', value: 4 }, { label: '六月', value: 5 },
-        { label: '七月', value: 6 }, { label: '八月', value: 7 }, { label: '九月', value: 8 },
-        { label: '十月', value: 9 }, { label: '十一月', value: 10 }, { label: '十二月', value: 11 }
-      ],
+      monthArr: [],
       yearArr: [],
-      currArr: []
+      yearRange: 10,
+      currArr: [],
+      selectType: ['single', 'mutiple', 'range'],
+      calendarType: ['week', 'date', 'month', 'year', 'yearRange', 'datetime'],
     }
   },
   mounted() {
-
+    this.initYearMonth()
   },
   methods: {
+    initYearMonth() {
 
+    }
   }
 }
 </script>
